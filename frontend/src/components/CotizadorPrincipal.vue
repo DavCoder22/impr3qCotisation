7cb5294907b941435b6bc1616cd12ae90d1b25ed
DavCoder22 <davidelpatron22@gmail.com>
2024-12-14 08:58:39 -0500

    feat: :sparkles: views integration
    the forntend view integrate with backend
    
    any

--- frontend/src/components/CotizadorPrincipal.vue
+++ frontend/src/components/CotizadorPrincipal.vue
@@ -41,9 +41,11 @@
 
 <script>
 import axios from "axios";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient("https://tu-supabase-url.supabase.co", "tu-clave-publica");
+const supabaseUrl = 'https://helaklijjobvxzgysrmn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhlbGFrbGlqam9idnh6Z3lzcm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3MjY1MjYsImV4cCI6MjA0OTMwMjUyNn0.CUotpjQMW2FbI1aWvzqjFNVMA21f6sLhcSD_5e3PWyM';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default {
   data() {
